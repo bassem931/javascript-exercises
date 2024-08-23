3f27021572b82795f6b6e2a5f9a99f3366b64c66
Bassem Taher <87501973+bassem931@users.noreply.github.com.>
2024-08-24 00:06:49 +0300

    feat: complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+if(typeof(num1) !== "number" || typeof(num2) !=="number"){
+    return "ERROR";
+}
 
+if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+    return "ERROR";
+}
+
+if(!((num1>=0) && (num2>=0))){
+    return "ERROR";
+}
+
+let largeNum = 0;
+let smallNum = 0;
+if(num1>num2){
+    largeNum = num1;
+    smallNum = num2;
+} else {
+    largeNum = num2;
+    smallNum = num1;
+}
+
+let sum = 0;
+for (let i = smallNum; i <= largeNum ; i++) {
+    sum += i;
+}
+return sum;
 };
 
 // Do not edit below this line
