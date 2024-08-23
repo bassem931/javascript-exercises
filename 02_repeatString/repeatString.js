b0f9b21e6af556b5094204768a8d78a89191c7be
Bassem Taher <87501973+bassem931@users.noreply.github.com.>
2024-08-23 22:59:00 +0300

    feat: complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let resStr = "";
+    if(!(num >=0)){
+        return "ERROR";
+    }
+for (let i = 0; i < num; i++) {
+    resStr += str;
+}
+return resStr;
 };
 
 // Do not edit below this line
